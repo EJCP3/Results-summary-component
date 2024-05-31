18beaa1632fa19ee69612fdf0ded3c3dd7b1ce79
EJCP3 <euddycabrera03@gmail.com>
2024-05-30 21:54:35 -0400

    v-movil

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      './src/**/*.{html,js}',
-    ],
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",    ],
     theme: {
       extend: {
    
@@ -12,16 +12,25 @@ module.exports = {
 
         colors: {
           ///Primary
+          LightRedBG: "hsl(0, 100%, 67%, 0.05)",
           LightRed: "hsl(0, 100%, 67%)",
+          OrangeyYellowBG: "hsl(39, 100%, 56%, 0.05)",
           OrangeyYellow: "hsl(39, 100%, 56%)",
+          GreenTealBG: "hsl(166, 100%, 37%, 0.05)",
           GreenTeal: "hsl(166, 100%, 37%)",
+          CobaltBlueBG: "hsl(234, 85%, 45%, 0.05)",
           CobaltBlue: "hsl(234, 85%, 45%)",
+          
 
            ///Gradients
-            LightSlateBlue: "hsl(252, 100%, 67%)",
+            LightSlateBlue: "hsl(252, 100%, 62%)",
             LightRoyalBlue: "hsl(241, 81%, 54%)",
             VioletBlue: "hsla(256, 72%, 46%, 1)",
-            PersianBlue : "hsla(241, 72%, 46%, 0)",
+            PersianBlue: "hsla(241, 80%, 7%, 0.3)",
+
+
+            primary1: 'hsla(254, 100%, 10%, 0.7)',  // Color con transparencia
+            primary2: 'hsla(256, 72%, 80%, 0.3)',   // Color con transparencia
 
             ///Neutral
             White: "hsl(0, 0%, 100%)",
@@ -30,8 +39,17 @@ module.exports = {
              DarkGrayBlue: "hsl(224, 30%, 27%)",
         },
 
+        backgroundImage: {
+          'gradient-to-b': 'linear-gradient(to bottom, var(--tw-gradient-stops))',
+        },
+        gradientColorStops: theme => ({
+          'start': 'hsla(256, 72%, 46%, 1)',
+          'end': 'hsla(241, 72%, 46%, 0)',
+        }),
+
+
         screens: {
-          'pc': '380px',
+          'pc': '600px',
         },
       },
     },
